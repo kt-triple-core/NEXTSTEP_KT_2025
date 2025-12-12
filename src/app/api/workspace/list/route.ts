71d0e9385b2e188bdc34f41f8da38df0c8427837
rkddusen <k99endus@naver.com>
2025-12-12 17:24:19 +0900

    [feat]_12/12-내 워크스페이스 리스트 가져오기

---
+++ src/app/api/workspace/list/route.ts
@@ -0,0 +1,46 @@
+import { NextResponse } from 'next/server'
+import { supabase } from '@/shared/libs/supabaseClient'
+import { getServerSession } from 'next-auth'
+
+export const GET = async () => {
+  try {
+    // 유저 검증
+    const session = await getServerSession(authOption)
+    if (!session) {
+      return NextResponse.json(
+        { error: 'Unauthorized', data: {} },
+        { status: 401 }
+      )
+    }
+
+    const userId = session.user?.id
+    if (!userId) {
+      return NextResponse.json(
+        { error: 'Invalid session', data: {} },
+        { status: 400 }
+      )
+    }
+
+    const { data, error } = await supabase
+      .from('workspaces')
+      .select('*')
+      .eq('user_id', userId)
+
+    if (error) {
+      return NextResponse.json(
+        { error: 'Failed to fetch workspaces', details: error.message },
+        { status: 500 }
+      )
+    }
+
+    return NextResponse.json({
+      error: null,
+      data,
+    })
+  } catch (error) {
+    return NextResponse.json(
+      { error: 'Internal server error', message: (error as Error).message },
+      { status: 500 }
+    )
+  }
+}
