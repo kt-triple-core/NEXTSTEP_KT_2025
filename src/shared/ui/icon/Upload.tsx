b801bca96ea8f981d2072e5428763e6853faaf6b
kimJAVAN <ookim7717@gmail.com>
2025-12-17 11:23:39 +0900

    [chore]_12/17- 아이콘 추가 업로드/공유

---
+++ src/shared/ui/icon/Upload.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+const Upload = ({ size = 24 }) => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width={size}
+    height={size}
+    viewBox="0 0 24 24"
+    fill="none"
+    strokeWidth="2"
+    strokeLinecap="round"
+    strokeLinejoin="round"
+    className="stroke-foreground"
+  >
+    <path d="M3 15v4c0 1.1.9 2 2 2h14a2 2 0 0 0 2-2v-4M17 8l-5-5-5 5M12 4.2v10.3" />
+  </svg>
+)
+export default Upload
